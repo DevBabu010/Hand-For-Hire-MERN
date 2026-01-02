769e1eb481573f5008709bd036c554a7ea2f3b9f
Devbabu010 <neoshail010@gmail.com>
2026-01-03 00:53:39 +0530

    Api testing

--- hand-for-hire/src/App.jsx
+++ hand-for-hire/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
-
+import "./App.css"; // ← this must exist
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Banner from "./components/Banner";
@@ -10,7 +10,8 @@ import Wedding from "./components/Wedding";
 import Gemstones from "./components/pages/Gemstones";
 import Loginform from "./components/Loginform";
 import Hamburgermenu from "./components/Hamburgermenu";
-import "./App.css"; // ← this must exist
+import Apitest from "./components/Apitest";
+
 
 
 function App() {
@@ -48,6 +49,12 @@ function App() {
         <Route 
         path="/gemstones" 
         element={<Gemstones />         
+        } 
+        />
+
+        <Route 
+        path="/apitest" 
+        element={<Apitest />         
         } 
         />
       </Routes>
---
+++ hand-for-hire/src/components/Apitest.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import "./Apitest.css";
+
+function Users() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch("https://dummyjson.com/users")
+        .then(res => res.json())
+        .then(data => setUsers(data.users));
+    }, []);
+
+    return (
+        <div className="user-container">
+        {users.map(user => (
+            <div className="user-card" key={user.id}>
+            <img src={user.image} alt={user.firstName} />
+            <h3>{user.firstName} {user.lastName}</h3>
+            <p>{user.email}</p>
+            <p>Age: {user.age}</p>
+            </div>
+        ))}
+        </div>
+    );
+}
+
+export default Users;
